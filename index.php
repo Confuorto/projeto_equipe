dd45b05e96894e7884702219955f1412e7558607
vitorconfuorto <vitorconfuorto@localhost>
2022-08-26 11:47:27 -0300

    Meu primeiro commit!

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit th
     </head>
     <body>
         <?php
-        
+            echo "Ola mundo";
         ?>
     </body>
 </html>
